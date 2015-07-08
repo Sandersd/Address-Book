2dfb6f1a82590761be0e4899e75e711348a69d25
Dylan Sanders <sandersd27@gmail.com>
2015-07-07 17:00:00 -0700

    initial commit

---
+++ contact.js
@@ -0,0 +1,20 @@
+var Contact = {};
+
+Contact.parseName = function(str) {
+  return str.split(',')[0].trim();
+}
+
+Contact.parseNumber = function(str) {
+ return str.split(',')[1].trim();
+}
+
+Contact.createContact = function(str) {
+  var contact = {
+    name: this.parseName(str),
+    number: this.parseNumber(str)
+  }
+
+  return contact;
+}
+
+module.exports = Contact;
---
+++ test.js
@@ -0,0 +1,5 @@
+var MyContact = require('./contact');
+
+var contact = MyContact.createContact("John Smith,604-123-9090");
+
+console.log(contact);
